13886ab2d807c45908d8ac890ae18a13d1d8f70c
Calvin Suiter <cdsuiter@yahoo.com>
2025-06-13 23:55:34 -0400

    Added function and inner function code. Checked that code passed tests

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  const maxAttempts = 3;
 
+  const handleLoginAttempt = (passwordAttempt) => {
+    if (attemptCount >= maxAttempts) {
+      return "Account locked due to too many failed login attempts"
+    }
+    
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful"
+    }
+    else {
+      attemptCount++;
+      return `Attempt ${attemptCount}: Login failed`
+    }
+  }
 
+  return handleLoginAttempt
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+}
